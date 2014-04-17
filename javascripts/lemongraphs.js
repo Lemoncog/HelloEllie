e37c53b23b9ee1fc3e038b0332711e1e4c74ef2d
Jason Leeming <jasonleeming@Jason-Leemings-MacBook-Pro.local>
2014-04-17 20:39:25 +0100

    Adding line plotting below blobs

--- javascripts/lemongraphs.js
+++ javascripts/lemongraphs.js
@@ -8,8 +8,8 @@ function drawAxis(isX, width, height) {
 	drawLine(0, height, isX ? width : 0, isX ? height : 0 , "rgba(0, 0, 0, 0.5)", 5);
 }
 
-function drawLine(x, y, width, height, color, lineWidth) {
-	var line = two.makeLine(x, y,  width, height);
+function drawLine(x, y, x2, y2, color, lineWidth) {
+	var line = two.makeLine(x, y, x2, y2);
 	line.linewidth = lineWidth;
 	line.stroke = color;
 }
@@ -46,6 +46,39 @@ function plotBlob(x, y, radius) {
 	circle.linewidth = 5;
 }
 
+function plotDummyData() {
+	var color = "rgba(0, 0, 0, 0.5)";
+	
+	var plottedBlobs = [];
+	
+	for(var count = 0; count < 50; count++) {
+		var randomX = Math.random();
+		var randomY = Math.random();
+		
+		var x = GRAPH_WIDTH*randomX;
+		var y = GRAPH_HEIGHT*randomY;
+		
+		plottedBlobs.push({ x: x, y: y });
+	}	
+	
+	for(var count = 0; count < plottedBlobs.length; count++) {
+		var prevX = count > 0 ? plottedBlobs[count-1].x : 0;
+		var prevY = count > 0 ? plottedBlobs[count-1].y : 0;
+		
+		var x = plottedBlobs[count].x;
+		var y = plottedBlobs[count].y;
+		
+		drawLine(x, y, prevX, prevY, color, 1);
+	}
+	
+	for(var count = 0; count < plottedBlobs.length; count++) {
+		var x = plottedBlobs[count].x;
+		var y = plottedBlobs[count].y;
+		
+		plotBlob(x, y, 10*Math.random());
+	}
+}
+
 // Make an instance of two and place it on the page.
 
 var elem = document.getElementById('draw-shapes').children[0];
@@ -58,14 +91,7 @@ var two = new Two(params).appendTo(elem);
 drawXAxis();
 drawYAxis();
 drawGrid(10,10);
-
-
-for(var count = 0; count < 50; count++) {
-	var randomX = Math.random();
-	var randomY = Math.random();
-	
-	plotBlob(GRAPH_WIDTH*randomX, GRAPH_HEIGHT*randomY, 10*Math.random());
-}
+plotDummyData();
 
 var circle = two.makeCircle(72, 100, 50);
 var rect = two.makeRectangle(213, 100, 100, 100);
